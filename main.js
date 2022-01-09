365d413ad10406d244cd86412a34ae3be338c54a
sabbytech <80594124+sabbytech@users.noreply.github.com>
2022-01-09 16:17:00 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-//Next Class
+Webcam.set({
+width: 385,
+height:300,
+image_format: "png"
+png_quality: 90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+
+function Capture(){
+    
+    Webcam.snap(function(data_url)
+    {document.getElementById("result").innerHTML = '<img id= " object_image" src= "'+data_url+'">';});
+
+}
+
+console.log('ml5 version:', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/hvhtNDXEf/model.json',modelLoaded)
